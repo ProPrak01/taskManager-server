7e3eaddb4769378176c1799397ca02b6c664f2b2
Prakash <127179011+ProPrak01@users.noreply.github.com>
2024-05-16 21:18:10 +0530

    changesa

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -9,7 +8,6 @@ const port = 5001;
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
 mongoose.connect('mongodb+srv://ph1010ishard:abcd@cluster0.pwk1gyn.mongodb.net/', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -18,7 +16,6 @@ mongoose.connect('mongodb+srv://ph1010ishard:abcd@cluster0.pwk1gyn.mongodb.net/'
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 
-// Define the Task model
 const taskSchema = new mongoose.Schema({
   text: String,
   completed: Boolean,
@@ -30,13 +27,11 @@ app.get("/",cors(),(req,res)=>{
   res.send("hello this is server");
 })
 
-// Get all tasks
 app.get('/api/tasks', async (req, res) => {
   const tasks = await Task.find();
   res.send(tasks);
 });
 
-// Add a new task
 app.post('/api/tasks', async (req, res) => {
   const { text, time } = req.body;
   const task = new Task({ text, completed: false, time });
@@ -44,7 +39,6 @@ app.post('/api/tasks', async (req, res) => {
   res.send(task);
 });
 
-// Update a task
 app.put('/api/tasks/:id', async (req, res) => {
   const { id } = req.params;
   const { text, completed, time } = req.body;
@@ -57,7 +51,6 @@ app.put('/api/tasks/:id', async (req, res) => {
   res.send(task);
 });
 
-// Delete a task
 app.delete('/api/tasks/:id', async (req, res) => {
   const { id } = req.params;
   const task = await Task.findByIdAndDelete(id);
